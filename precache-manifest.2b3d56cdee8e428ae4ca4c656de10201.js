e6ff115e7b64035650efb52efca7d110287fa6c9
Jatin <getjatin5120@gmail.com>
2022-07-15 01:25:00 +0530

    Updates

---
+++ precache-manifest.2b3d56cdee8e428ae4ca4c656de10201.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0fd5c059ee0199ddda7",
+    "url": "/Jatin-Portfolio/static/js/main.c0fd5c05.chunk.js"
+  },
+  {
+    "revision": "266190d1fb51ad12d0a4",
+    "url": "/Jatin-Portfolio/static/js/1.266190d1.chunk.js"
+  },
+  {
+    "revision": "ee9b45b18c21db228c25",
+    "url": "/Jatin-Portfolio/static/js/2.ee9b45b1.chunk.js"
+  },
+  {
+    "revision": "89c989238a576f3e86dc",
+    "url": "/Jatin-Portfolio/static/js/runtime~main.89c98923.js"
+  },
+  {
+    "revision": "d0802f9a79fb2e387c86d44ce2bc2110",
+    "url": "/Jatin-Portfolio/static/media/duck.d0802f9a.riv"
+  },
+  {
+    "revision": "58b5568e30eb337abe29c47b1f83fec9",
+    "url": "/Jatin-Portfolio/static/media/menu.58b5568e.riv"
+  },
+  {
+    "revision": "c21a14d2253b2971fb12c5f57967efd9",
+    "url": "/Jatin-Portfolio/static/media/loading.c21a14d2.riv"
+  },
+  {
+    "revision": "91d5312ad6690c1802f9f010dc1f33ae",
+    "url": "/Jatin-Portfolio/static/media/jatinLogo.91d5312a.svg"
+  },
+  {
+    "revision": "1fadcf14e6f858d98209896f9a7e6301",
+    "url": "/Jatin-Portfolio/index.html"
+  }
+];
